5ea43fa1aa8c38e56d033b6ac6a9e4cdf7f9ba17
Adewuyi Marvellous <marveladeadewuyi@gmail.com>
2026-04-28 14:58:53 +0100

    fix: ensure LeftPanel height is set to 100vh for consistent layout

--- src/components/LeftPanel.jsx
+++ src/components/LeftPanel.jsx
@@ -24,6 +24,7 @@ const LeftPanel = ({ head, p, texthead, text1, text2, text3, text4, style, pStyl
         gap: '4em',
         alignItems: 'start',
         justifyContent: 'start',
+        height: '100vh',
 
     };
 
